56428d1f15ecfd04a5595366d80e1505cb85008c
JustLinton <530065614@qq.com>
2022-03-20 17:17:36 +0800

    feat: backend reg api

---
+++ source/music-client/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <!--轮播图-->
+  <el-carousel class="swiper-container" type="card" height="20vw" :interval="4000">
+    <el-carousel-item v-for="(item, index) in swiperList" :key="index">
+      <img :src="item.picImg" />
+    </el-carousel-item>
+  </el-carousel>
+  <!--热门歌单-->
+  <play-list class="section" title="歌单" path="song-sheet-detail" :playList="songList"></play-list>
+  <!--热门歌手-->
+  <play-list class="section" title="歌手" path="singer-detail" :playList="singerList"></play-list>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import PlayList from "@/components/PlayList.vue";
+import { swiperList } from "@/enums";
+import { HttpManager } from "@/api";
+
+const songList = ref([]); // 歌单列表
+const singerList = ref([]); // 歌手列表
+
+try {
+  HttpManager.getSongList().then((res) => {
+    songList.value = (res as any[]).sort().slice(0, 10);
+  });
+
+  HttpManager.getAllSinger().then((res) => {
+    singerList.value = (res as any[]).sort().slice(0, 10);
+  });
+} catch (error) {
+  console.error(error);
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/css/var.scss";
+
+.swiper-container {
+  width: 90%;
+  margin: auto;
+  padding-top: 100px;
+  img {
+    width: 100%;
+  }
+}
+
+.section {
+  width: 100%;
+  margin-top: 20px;
+  padding: $content-padding;
+  background-color: $color-white;
+  box-sizing: border-box;
+}
+</style>
---
+++ source/music-manage/src/components/layouts/YinAside.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="sidebar">
+    <el-menu
+      class="sidebar-el-menu"
+      background-color="#545c64"
+      text-color="#B0B3B2"
+      active-text-color="#ffffff"
+      default-active="2"
+      router
+      :collapse="collapse"
+    >
+      <el-menu-item index="info">
+        <el-icon><Document /></el-icon>
+        <span>系统首页</span>
+      </el-menu-item>
+      <el-menu-item index="consumer">
+        <el-icon><Document /></el-icon>
+        <span>用户管理</span>
+      </el-menu-item>
+      <el-menu-item index="singer">
+        <el-icon><Document /></el-icon>
+        <span>歌手管理</span>
+      </el-menu-item>
+      <el-menu-item index="songList">
+        <el-icon><Document /></el-icon>
+        <span>歌单管理</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Document } from "@element-plus/icons-vue";
+import emitter from "@/utils/emitter";
+
+const collapse = ref(false);
+emitter.on("collapse", (msg) => {
+  collapse.value = msg;
+});
+</script>
+
+<style scoped>
+.sidebar {
+  display: block;
+  position: absolute;
+  background-color: #334256;
+  left: 0;
+  top: 70px;
+  bottom: 0;
+  overflow-y: scroll;
+}
+.sidebar::-webkit-scrollbar {
+  width: 0;
+}
+.sidebar-el-menu:not(.el-menu--collapse) {
+  width: 150px;
+}
+.sidebar > ul {
+  height: 100%;
+}
+</style>
