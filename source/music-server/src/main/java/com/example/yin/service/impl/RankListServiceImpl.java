1e4e2dcbeada859961593bdac3d7a89aeb405b19
JustLinton <530065614@qq.com>
2022-03-20 17:17:36 +0800

    feat: backend reg api

---
+++ source/music-server/src/main/java/com/example/yin/service/impl/RankListServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.yin.service.impl;
+
+import com.example.yin.dao.RankListMapper;
+import com.example.yin.domain.RankList;
+import com.example.yin.service.RankListService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Administrator
+ */
+@Service
+public class RankListServiceImpl implements RankListService {
+
+    private static final Logger logger = LogManager.getLogger(RankListServiceImpl.class);
+
+    @Autowired
+    private RankListMapper rankMapper;
+
+    @Override
+    public int rankOfSongListId(Long songListId) {
+        // 评分总人数如果为 0，则返回0；否则返回计算出的结果
+        int rankNum = rankMapper.selectRankNum(songListId);
+        return (rankNum <= 0) ? 0 : rankMapper.selectScoreSum(songListId) / rankNum;
+    }
+
+    @Override
+    public boolean addRank(RankList rankList) {
+
+        return rankMapper.insertSelective(rankList) > 0;
+    }
+}
