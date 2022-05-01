6e5ca57c7cdc6d86367964e0cc3699629353daa2
JustLinton <530065614@qq.com>
2022-05-01 10:23:48 +0800

    add: song lists

--- source/music-server/src/main/java/com/example/yin/YinMusicApplication.java
+++ source/music-server/src/main/java/com/example/yin/YinMusicApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// spring
+
 @SpringBootApplication
 @MapperScan("com.example.yin.dao")
 public class YinMusicApplication {
